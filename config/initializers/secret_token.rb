cc727b842ed128367a9b28d096799c6e4ee967e7
片桐瑞樹 <katagirimizuki@katagirimizuki-no-MacBook-Air.local>
2013-12-10 03:03:51 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practice::Application.config.secret_token = '1a8392c6df5e88d095f43d7806016aa0dfd1feaeb6629b34bd3ba73d271bafd5695617635d8fd9ccd76d9e6b85ac62a94a7d74926a6710589e26f542ba71a828'
